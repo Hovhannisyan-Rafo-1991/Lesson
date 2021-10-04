de5bc5ec8af408d8c1ee6da959da9ffcd4a4dd6a
Rafael <Rafo.gyumri@mail.ru>
2021-10-04 20:20:48 +0400

    add

--- src/lesson3/CharDemo2.java
+++ src/lesson3/CharDemo2.java
@@ -3,10 +3,8 @@ package lesson3;
 public class CharDemo2 {
     public static void main(String[] args) {
         char ch1;
-
         ch1 = 'x';
         System.out.println("ch1 + сh1");
-
         ch1++;
         System.out.println("ch1 + ch1");
     }
